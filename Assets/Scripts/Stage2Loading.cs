6cb6cd52ba36714d3c6b69755ec17dd994af22d7
moonyijeh2008 <163096793+moonyijeh2008@users.noreply.github.com>
2024-12-08 00:00:25 +0900

    commit

--- Assets/Scripts/Stage2Loading.cs
+++ Assets/Scripts/Stage2Loading.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stage1Loading : MonoBehaviour
+public class Stage2Loading : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
